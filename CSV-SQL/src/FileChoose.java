f58f222a30ff0243eac7a17bb588dbb8cadc0922
Davischw26 <97300595+Davischw26@users.noreply.github.com>
2022-03-07 10:14:40 +0100

    Add files via upload

---
+++ CSV-SQL/src/FileChoose.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import javafx.application.Application;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+
+public class FileChoose extends Application 
+{
+    private static File file;
+    private String fileType = null;
+       
+    public void setFileType(String FileType)
+    {
+        fileType = FileType;
+    }
+    
+    @Override
+    public void start(Stage primaryStage) 
+    {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+            fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter(fileType, "*."+fileType));
+            file = fileChooser.showOpenDialog(primaryStage);           
+    }
+    
+    public String getFilePath()
+    {
+        String path = file.getPath();
+        return path;
+    }
+}
---
+++ CSV-SQL/src/StartApplication.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class StartApplication extends Application {
+      
+    public static void main(String[] args)
+    {
+
+        StartApplication stap = new StartApplication();
+
+
+        try
+        {
+            stap.start(null);
+        } 
+        catch (Exception ex) 
+        {
+            
+        }
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
